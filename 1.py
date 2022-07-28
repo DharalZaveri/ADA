b680714d0e7d9e647c05c1cceb72c399af81d496
DharalZaveri <zaveridharal13@gmail.com>
2022-07-28 15:56:05 +0530

    1.py

---
+++ 1.py
@@ -0,0 +1,16 @@
+def swap(s):
+    size = len(s)
+
+    temp = s[0]
+    s[0] = s[len(s)-1]
+    s[len(s)-1] = temp
+
+    return s
+
+s = [1,2,3,4,5,6,7]
+
+print(f"Before swap is {s}")
+a = swap(s)
+
+print(f"After swap is {a}")
+    
