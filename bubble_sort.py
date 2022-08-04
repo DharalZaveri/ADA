906de3cf9e2f7af79cc3368391038370c1b8883c
Dharal zaveri <102239285+DharalZaveri@users.noreply.github.com>
2022-08-04 15:09:57 +0530

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -7,6 +7,11 @@ def bubble_sort(ar):
                 ar[j+1] = temp
 
 
+ar = []
+for i in range(0,9):
+    a = int(input("Enter Numbers for bubble_sort: "))
+    ar.append(a)
+print("Befor Bubble_sort ",ar)
 arr = [2,6,3,1,7]
-bubble_sort(arr)
-print(arr)
+bubble_sort(ar)
+print(f"After Bubble_sort{ ar}")
